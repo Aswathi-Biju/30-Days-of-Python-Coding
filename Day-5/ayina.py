a860c89b8bd027103c1aa88c8bfb64d8371c2642
ayinasara <ayinasarajacob10@gmail.com>
2024-11-16 21:19:11 +0530

    Add files via upload

---
+++ Day-5/ayina.py
@@ -0,0 +1,10 @@
+temp= float(input("Enter the temperature in Celsius "))
+
+if temp >= 30:
+    print("It's hot!")
+elif 30> temp >= 20  :
+    print("It's warm!")
+elif 20>temp >= 10   :
+    print("It's cool")
+else:
+    print("It's cold")
