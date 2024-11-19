6521c9d113b3ed501a33d4dfc7185f3b7f79e166
bichusasi <bichusasi2028@cs.sjcetpalai.ac.in>
2024-11-19 21:26:11 +0530

    Add files via upload

---
+++ Day-8/Bichu_Sasi.py
@@ -0,0 +1,10 @@
+number= 0
+while True:
+    number = int(input("Enter a number (it can be greater than or less than 0 ):"))
+    if number > 0:
+        print("Thank You.")
+        break
+    else:
+        print("Try Again.")
+
+
