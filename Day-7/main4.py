440a1b8bb42e15314804c9cec52f38e8e9c564e1
bijinalex <bijinalex2028@cs.sjcetpalai.ac.in>
2024-11-20 22:43:39 +0530

    Add files via upload

---
+++ Day-7/main4.py
@@ -0,0 +1,8 @@
+     correct_password =input("Enter your password:")
+     while True:
+         password=input("Enter the password")
+         if password==correct_password:
+             print("Access granted")
+              break 
+         else:
+             print("Try Again")
