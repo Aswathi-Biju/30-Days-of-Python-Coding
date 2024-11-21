6ee21c14bb30bc6435bb7faba4f7fe1c9332b335
bijinalex <bijinalex2028@cs.sjcetpalai.ac.in>
2024-11-21 19:51:44 +0530

    Add files via upload

---
+++ Day-8/main5.py
@@ -0,0 +1,8 @@
+number=0
+while True :
+    number=int(input("Enter a number (it can be greater than or less than 0 ):"))
+    if number>0:
+        print("Thank You!")
+        break
+    else:
+         print("Try again")
