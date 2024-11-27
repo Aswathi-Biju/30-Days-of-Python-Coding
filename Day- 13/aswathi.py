5ee119ecdaaffc895d3174791ef21556c9429423
Aswathi Biju <aswathibiju8118@gmail.com>
2024-11-27 22:08:49 +0530

    Create aswathi.py

--- Day- 13/aswathi.py
+++ Day- 13/aswathi.py
@@ -1,12 +1,12 @@
 import random
-random_integer1 = random.randint(1,50)
+random_integer1 = random.randint(1,51)
 print("First Random Integer:", random_integer1)
-random_integer2 = random.randint(1,50)
+random_integer2 = random.randint(1,51)
 print("Second Random Integer:", random_integer2)
-random_integer3 = random.randint(1,50)
+random_integer3 = random.randint(1,51)
 print("Third Random Integer:", random_integer3)
-random_integer4 = random.randint(1,50)
+random_integer4 = random.randint(1,51)
 print("Fourth Random Integer:", random_integer4)
-random_integer5 = random.randint(1,50)
+random_integer5 = random.randint(1,51)
 print("Fifth Random Integer:", random_integer5)
 
