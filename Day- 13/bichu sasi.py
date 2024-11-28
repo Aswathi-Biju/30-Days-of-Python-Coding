615ca77a508ad0548b5ad6e2d7e44f6a8b30fb28
bichusasi <bichusasi2028@cs.sjcetpalai.ac.in>
2024-11-28 22:13:49 +0530

    Add files via upload

---
+++ Day- 13/bichu sasi.py
@@ -0,0 +1,11 @@
+Python 3.8.10 (default, Mar 15 2022, 12:22:08) 
+[GCC 9.4.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: /home/user/python/bichu_sasi.py ===================
+First Random Indiger:  21
+Second Random Indiger:  50
+Third Random Indiger:  38
+Fourth Random Indiger:  2
+Fifth Random Indiger:  27
+>>> 
