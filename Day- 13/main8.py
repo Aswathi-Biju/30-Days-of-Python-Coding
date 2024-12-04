b7edd979b65e0fe0de2a2a3e0da6f2f3e599ca7e
bijinalex <bijinalex2028@cs.sjcetpalai.ac.in>
2024-12-04 19:56:37 +0530

    Add files via upload

---
+++ Day- 13/main8.py
@@ -0,0 +1,11 @@
+import random
+random_integer1 = random.randint(1,50)
+print("First Random Integer:",random_integer1)
+random_integer2 = random.randint(1,50)
+print("Second Random Integer:", random_integer2)
+random_integer3 = random.randint(1,50)
+print("Third Random Integer:", random_integer3)
+random_integer4 = random.randint(1,50)
+print("Fourth Random Integer:", random_integer4)
+random_integer5 = random.randint(1,50)
+print("Fifth Random Integer:", random_integer5)
