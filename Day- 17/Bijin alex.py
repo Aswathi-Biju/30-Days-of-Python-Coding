45a24fb24470786dda8189f1570cd156aada0617
bijinalex <bijinalex2028@cs.sjcetpalai.ac.in>
2024-12-28 12:49:54 +0530

    Add files via upload

---
+++ Day- 17/Bijin alex.py
@@ -0,0 +1,9 @@
+fav_tuple = ("Blue","Black","Dog","Cat","Car")
+print("First element:",fav_tuple[0], "Last element:",fav_tuple[-1])
+print("The first 3 elements:",fav_tuple[ :3])
+tuple = ("Red","Orange","Yellow")
+print("Combined tuple:",fav_tuple+tuple)
+repeated = fav_tuple * 3
+print("Repeated tuple:",repeated)
+print("Is Black in the tuple?","Black" in fav_tuple)
+print("Length of the tuple:",len(fav_tuple))
